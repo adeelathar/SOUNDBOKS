af12dffb124b85e75848d6424ba1cd1693f28f4d
adeel <adeel.athar@echofreelance.com>
2016-09-15 22:33:27 +0500

    Created and styled header and navigation

--- wp-content/themes/SOUNDBOKS/header.php
+++ wp-content/themes/SOUNDBOKS/header.php
@@ -11,41 +11,72 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'soundboks' ); ?></a>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
         
-
-            <nav class="navbar navbar-default "  >
-            <div class="container">
-              <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-                <a class="navbar-brand " href="<?php echo esc_url( home_url( '/' ) ); ?>" >
-                    <img src="<?php echo get_template_directory_uri().'/assets/images/logo.png';  ?>" />
-                </a>
-              </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <?php wp_nav_menu( array( 'menu_class'      => 'menu nav navbar-nav pull-right','theme_location' => 'primary', 'menu_id' => 'site-navigation' ) ); ?>
-          
-                </div>
-            </div>
-          </nav>
+<?php wp_head(); ?>
+    </head>
+    
+    <body <?php body_class(); ?>>
+        <div id="page" class="site">
+            <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'soundboks' ); ?></a>
             
-		
+            
+            <nav class="navbar  "  >
+                <div class="container">
+                    
+                    <div class='row'>
+                        <div class="col-sm-3">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand " href="<?php echo esc_url( home_url( '/' ) ); ?>" >
+                                    <img src="<?php echo get_template_directory_uri().'/assets/images/logo.png';  ?>" />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-sm-9">
+                            <div class='row'>
+                                <div class="col-sm-4">
+                                    <ul class="menu nav navbar-nav pull-right countries">
+                                        <li><a href='#'>US</a></li>
+                                        <li class='active'><a href='#'>EUROPE</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-8">
+                                    <div id="navbar" class="navbar-collapse collapse">
+                                        
+                                                        <?php 
+                                                        $cart_circle = "";
+                                                        if(WC()->cart->get_cart_contents_count()>0)
+                                                        {
+                                                             $cart_circle = "<div class='circle'></div>";
+                                                        }
+                                                       
+                                                        $cart_link = '<li class="cart_icon"><a href="'.wc_get_cart_url().'"><i class="glyphicon glyphicon-shopping-cart">'.$cart_circle.'</i></a></li>';
+                                                        
+                                                        
+                                                        wp_nav_menu( array( 'menu_class'      => 'menu nav navbar-nav pull-right','theme_location' => 'primary', 'menu_id' => 'site-navigation', 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s '.$cart_link.'</ul>' ) ); ?>
+                                        
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            
+                            
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </nav>
+            
 
-
-
-	<div id="content" class="site-content">
+            <div id="content" class="site-content">
